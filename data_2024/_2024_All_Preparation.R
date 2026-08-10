b2df38a635e8d347a726646144f0799dd1820e82
Hank Helmers <89431423+HankHelmers@users.noreply.github.com>
2026-08-10 11:27:51 -0500

    Renamed "Raw_Datasets" as "source_data" and created output_data for the newly produced

--- data_2024/_2024_All_Preparation.R
+++ data_2024/_2024_All_Preparation.R
@@ -3,7 +3,7 @@ library(tidyverse)
 
 # ---------------------------------
 # Import data
-health_assess_2024 <- read_csv("Raw_Datasets/2024 Wild Collected Butternut DNA Leaf Samples - Sheet1.csv")
+health_assess_2024 <- read_csv("source_data/2024 Wild Collected Butternut DNA Leaf Samples - Sheet1.csv")
 
 # Select only WCP observations (first 66)
 health_assess_2024 <-  filter(health_assess_2024, health_assess_2024$`Site number or initial` == "WCP")
@@ -19,7 +19,8 @@ source("data_2024/1_Data_Processing/Typing.R")
 # 1b. Photos
 source("data_2024/0_Photos/Sync_Photos_With_Renaming_Info.R") 
 
+# Re-order columns 
 health_assess_2024 <- health_assess_2024 %>% select(site, site_owner, date, plant_number, plant_initials, first_photo, last_photo, camera, seedling_y_n, everything())
 
 # Write to CSV
-write.csv(health_assess_2024, "cleaned_2024_Health_Assessments.csv", row.names = FALSE)
+write.csv(health_assess_2024, "output_data/1_processed_2024_Health_Assessments.csv", row.names = FALSE)
