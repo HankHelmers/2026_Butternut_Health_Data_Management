6e382f5f2a0092d0ada279f8e0d801015e649484
Hank Helmers <89431423+HankHelmers@users.noreply.github.com>
2026-08-19 09:20:54 -0500

    2025: Removed the genotyped black walnuts

--- data_2025&on/1_Processing/2025_June_July/Removals.R
+++ data_2025&on/1_Processing/2025_June_July/Removals.R
@@ -10,12 +10,16 @@ test_entry_5 = ymd_hms("2025-08-06 15:31:37")  # "test entry"
 test_entry_6 = ymd_hms("2025-08-06 15:38:19")  # "test entry" 
 
 # Removing black walnuts
-black_walnut = ymd_hms("2025-06-19 10:46:22") # "this is black walnut!"
+# WCP 7 
+black_walnut_1 = ymd_hms("2025-06-19 10:46:22") # "this is black walnut!"
 
-# Note that black walnuts are not removed here because we need to keep those 
-# data points around for thorough photo data tracking. 
-#
-# Black walnut data points are removed for the health assessment data, however.
-remove_entries = c(black_walnut, test_entry_1, test_entry_2, test_entry_3, test_entry_4, test_entry_5, test_entry_6)
+# WCP 8
+black_walnut_2 = ymd_hms("2025-06-19 11:59:05") # Notes: This seems to be a black walnut!
+
+# WCP 24
+black_walnut_3 = ymd_hms("2025-06-26 09:40:06") # This one was confirmed with the genotypings 
+
+
+remove_entries = c(black_walnut_1, black_walnut_2, black_walnut_3, test_entry_1, test_entry_2, test_entry_3, test_entry_4, test_entry_5, test_entry_6)
 
 june_health_assess <- june_health_assess %>% filter(!timestamp %in% remove_entries)
